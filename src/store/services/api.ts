47d712e53af7f99c169b602eb8e78574e8f9708f
stclamp <whatisclamp@gmail.com>
2023-09-08 00:10:48 +0300

    fix: change base url in api.ts

--- src/store/services/api.ts
+++ src/store/services/api.ts
@@ -8,7 +8,7 @@ import { IError } from '@/src/types/Types';
 
 const apiService = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3001/',
+    baseUrl: import.meta.env.VITE_BACKEND_LINK,
     credentials: 'include',
   }) as BaseQueryFn<string | FetchArgs, unknown, IError>,
   endpoints: () => ({}),
